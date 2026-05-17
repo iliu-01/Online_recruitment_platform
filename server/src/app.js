c0a2b78fad54dc69f5bfc26f93258891008a26fd
iliu-01 <2694135791@qq.com>
2026-05-17 17:56:43 +0800

    fix: add .gitignore for .env files, fix error handler statusCode, fix dotenv path

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(err.status || 500).json({
+  res.status(err.status || err.statusCode || 500).json({
     error: err.message || 'Internal Server Error',
   });
 });
